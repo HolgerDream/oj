1e13351f091cb045138a46875cb5b0027009f18d
holger <dreamorgin@qq.com>
2019-07-17 17:14:57 +0800

    leetcode P13

---
+++ src/com/holger/leetcode/P13/罗马数转整数/Solution.java
@@ -0,0 +1,45 @@
+package com.holger.leetcode.P13.罗马数转整数;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Holger
+ * @date 2019/7/17
+ */
+public class Solution {
+
+    /**
+     * 首先将所有的可能枚举出来，然后判断下两个字符是否在map中
+     * @param s
+     * @return
+     */
+    public int romanToInt(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("IV", 4);
+        map.put("V", 5);
+        map.put("IX", 9);
+        map.put("X", 10);
+        map.put("XL", 40);
+        map.put("L", 50);
+        map.put("XC", 90);
+        map.put("C", 100);
+        map.put("CD", 400);
+        map.put("D", 500);
+        map.put("CM", 900);
+        map.put("M", 1000);
+
+        int ans = 0;
+        for(int i = 0;i < s.length();) {
+            if(i + 1 < s.length() && map.containsKey(s.substring(i, i+2))) {
+                ans += map.get(s.substring(i, i+2));
+                i += 2;
+            } else {
+                ans += map.get(s.substring(i, i+1));
+                i ++;
+            }
+        }
+        return ans;
+    }
+}
